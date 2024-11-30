09ddb7123b81d51401bce3ead69b4c665e1bbe5c
junho526 <jungjunho0526@naver.com>
2024-11-30 20:19:49 +0900

    update
    update roomms

---
+++ Form3.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace roomms
+{
+    partial class room
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rCapacitytxt = new System.Windows.Forms.TextBox();
+            this.mNumtxt = new System.Windows.Forms.TextBox();
+            this.rNumtxt = new System.Windows.Forms.TextBox();
+            this.Editbtn = new System.Windows.Forms.Button();
+            this.Delbtn = new System.Windows.Forms.Button();
+            this.rCleaningtxt = new System.Windows.Forms.ComboBox();
+            this.rStatustxt = new System.Windows.Forms.ComboBox();
+            this.Addbtn = new System.Windows.Forms.Button();
+            this.Backbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(50, 48);
+            this.DataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.RowHeadersWidth = 51;
+            this.DataGridView1.RowTemplate.Height = 27;
+            this.DataGridView1.Size = new System.Drawing.Size(646, 478);
+            this.DataGridView1.TabIndex = 0;
+            this.DataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(747, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "방 번호";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(747, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "방 상태";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(747, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "수용 인원";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(747, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "청소 여부";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(747, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "기계 번호";
+            // 
+            // rCapacitytxt
+            // 
+            this.rCapacitytxt.Location = new System.Drawing.Point(869, 170);
+            this.rCapacitytxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rCapacitytxt.Name = "rCapacitytxt";
+            this.rCapacitytxt.Size = new System.Drawing.Size(111, 25);
+            this.rCapacitytxt.TabIndex = 7;
+            // 
+            // mNumtxt
+            // 
+            this.mNumtxt.Location = new System.Drawing.Point(869, 300);
+            this.mNumtxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mNumtxt.Name = "mNumtxt";
+            this.mNumtxt.Size = new System.Drawing.Size(111, 25);
+            this.mNumtxt.TabIndex = 9;
+            // 
+            // rNumtxt
+            // 
+            this.rNumtxt.Location = new System.Drawing.Point(869, 44);
+            this.rNumtxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rNumtxt.Name = "rNumtxt";
+            this.rNumtxt.Size = new System.Drawing.Size(111, 25);
+            this.rNumtxt.TabIndex = 10;
+            // 
+            // Editbtn
+            // 
+            this.Editbtn.Location = new System.Drawing.Point(750, 418);
+            this.Editbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Editbtn.Name = "Editbtn";
+            this.Editbtn.Size = new System.Drawing.Size(80, 38);
+            this.Editbtn.TabIndex = 11;
+            this.Editbtn.Text = "수정";
+            this.Editbtn.UseVisualStyleBackColor = true;
+            this.Editbtn.Click += new System.EventHandler(this.Editbtn_Click);
+            // 
+            // Delbtn
+            // 
+            this.Delbtn.Location = new System.Drawing.Point(750, 488);
+            this.Delbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Delbtn.Name = "Delbtn";
+            this.Delbtn.Size = new System.Drawing.Size(80, 38);
+            this.Delbtn.TabIndex = 13;
+            this.Delbtn.Text = "삭제";
+            this.Delbtn.UseVisualStyleBackColor = true;
+            this.Delbtn.Click += new System.EventHandler(this.Delbtn_Click);
+            // 
+            // rCleaningtxt
+            // 
+            this.rCleaningtxt.FormattingEnabled = true;
+            this.rCleaningtxt.Items.AddRange(new object[] {
+            "Clean",
+            "Dirty"});
+            this.rCleaningtxt.Location = new System.Drawing.Point(869, 238);
+            this.rCleaningtxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rCleaningtxt.Name = "rCleaningtxt";
+            this.rCleaningtxt.Size = new System.Drawing.Size(111, 23);
+            this.rCleaningtxt.TabIndex = 14;
+            this.rCleaningtxt.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // rStatustxt
+            // 
+            this.rStatustxt.FormattingEnabled = true;
+            this.rStatustxt.Items.AddRange(new object[] {
+            "Available",
+            "Unavailable"});
+            this.rStatustxt.Location = new System.Drawing.Point(869, 111);
+            this.rStatustxt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rStatustxt.Name = "rStatustxt";
+            this.rStatustxt.Size = new System.Drawing.Size(111, 23);
+            this.rStatustxt.TabIndex = 15;
+            // 
+            // Addbtn
+            // 
+            this.Addbtn.Location = new System.Drawing.Point(901, 418);
+            this.Addbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(80, 38);
+            this.Addbtn.TabIndex = 16;
+            this.Addbtn.Text = "추가";
+            this.Addbtn.UseVisualStyleBackColor = true;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
+            // Backbtn
+            // 
+            this.Backbtn.Location = new System.Drawing.Point(901, 488);
+            this.Backbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Backbtn.Name = "Backbtn";
+            this.Backbtn.Size = new System.Drawing.Size(80, 38);
+            this.Backbtn.TabIndex = 17;
+            this.Backbtn.Text = "돌아가기";
+            this.Backbtn.UseVisualStyleBackColor = true;
+            this.Backbtn.Click += new System.EventHandler(this.Backbtn_Click);
+            // 
+            // room
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 578);
+            this.Controls.Add(this.Backbtn);
+            this.Controls.Add(this.Addbtn);
+            this.Controls.Add(this.rStatustxt);
+            this.Controls.Add(this.rCleaningtxt);
+            this.Controls.Add(this.Delbtn);
+            this.Controls.Add(this.Editbtn);
+            this.Controls.Add(this.rNumtxt);
+            this.Controls.Add(this.mNumtxt);
+            this.Controls.Add(this.rCapacitytxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "room";
+            this.Text = "room";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.room_FormClosed);
+            this.Load += new System.EventHandler(this.room_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox rCapacitytxt;
+        private System.Windows.Forms.TextBox mNumtxt;
+        private System.Windows.Forms.TextBox rNumtxt;
+        private System.Windows.Forms.Button Editbtn;
+        private System.Windows.Forms.Button Delbtn;
+        private System.Windows.Forms.ComboBox rCleaningtxt;
+        private System.Windows.Forms.ComboBox rStatustxt;
+        private System.Windows.Forms.Button Addbtn;
+        private System.Windows.Forms.Button Backbtn;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace roomms
+{
+    public partial class room : Form
+    {
+        new main Parent;
+        string sqlstr;    //쿼리문 저장 변수
+        DBClass dbc = new DBClass();
+
+        public room()
+        {
+            InitializeComponent();
+        }
+        public void room_header()
+        {
+            DataGridView1.Columns[0].HeaderText = "방 번호";
+            DataGridView1.Columns[1].HeaderText = "상태";
+            DataGridView1.Columns[2].HeaderText = "수용 인원";
+            DataGridView1.Columns[3].HeaderText = "청소 상태";
+            DataGridView1.Columns[4].HeaderText = "기계 번호";
+
+            DataGridView1.Columns[0].Width = 60;
+            DataGridView1.Columns[1].Width = 80;
+            DataGridView1.Columns[2].Width = 80;
+            DataGridView1.Columns[3].Width = 80;
+            DataGridView1.Columns[4].Width = 80;
+        }
+
+        private void room_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void room_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dbc.DB_ObjCreate(); //*****
+                dbc.DB_Open();//*****
+                dbc.DB_Access();//***
+
+                sqlstr = "Select * From room ORDER BY room_id ASC";
+                dbc.DCom.CommandText = sqlstr;
+                dbc.DA.SelectCommand = dbc.DCom;
+                dbc.DA.Fill(dbc.DS, "room");
+                dbc.DS.Tables["room"].Clear();
+                dbc.DA.Fill(dbc.DS, "room");
+                DataGridView1.DataSource = dbc.DS.Tables["room"].DefaultView;
+                room_header();
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+        }
+
+
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+
+        private void Editbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 수정할 행을 찾기 위한 PrimaryKey 설정
+                DataColumn[] primaryKey = new DataColumn[1];
+                primaryKey[0] = dbc.DS.Tables["room"].Columns["room_id"];
+                dbc.DS.Tables["room"].PrimaryKey = primaryKey;
+
+                // 선택된 행을 찾고 수정
+                DataRow rowToUpdate = dbc.DS.Tables["room"].Rows.Find(rNumtxt.Text); // 방 번호 기준으로 찾기
+                if (rowToUpdate != null)
+                {
+                    rowToUpdate["machine_id"] = mNumtxt.Text; // 기계 번호 텍스트박스
+                    rowToUpdate["status"] = rStatustxt.Text; // 상태 텍스트박스
+                    rowToUpdate["capacity"] = rCapacitytxt.Text; // 수용 인원 텍스트박스
+                    rowToUpdate["cleaning_status"] = rCleaningtxt.Text; // 청소 상태 텍스트박스
+
+                    // DB에 반영
+                    dbc.DA.Update(dbc.DS, "room");
+                    dbc.DS.AcceptChanges(); // 변경 사항 적용
+
+                    MessageBox.Show("방 정보가 수정되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("해당 방 번호를 찾을 수 없습니다.");
+                }
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+        }
+
+        private void Delbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 삭제할 행을 찾기 위한 PrimaryKey 설정
+                DataColumn[] primaryKey = new DataColumn[1];
+                primaryKey[0] = dbc.DS.Tables["room"].Columns["room_id"];
+                dbc.DS.Tables["room"].PrimaryKey = primaryKey;
+
+                // 선택된 행을 찾고 삭제
+                DataRow rowToDelete = dbc.DS.Tables["room"].Rows.Find(rNumtxt.Text); // 방 번호 기준으로 찾기
+                if (rowToDelete != null)
+                {
+                    rowToDelete.Delete();
+
+                    // DB에 반영
+                    dbc.DA.Update(dbc.DS, "room");
+                    dbc.DS.AcceptChanges(); // 변경 사항 적용
+
+                    MessageBox.Show("방 정보가 삭제되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("해당 방 번호를 찾을 수 없습니다.");
+                }
+            }
+            catch (DataException DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+            catch (Exception DE)
+            {
+                MessageBox.Show(DE.Message);
+            }
+        }
+
+        private void rStatustxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string roomNumber = rNumtxt.Text; // 방 번호 텍스트박스
+                string status = rStatustxt.SelectedItem.ToString(); // 선택된 상태
+                string machine_id = mNumtxt.Text; // 기계 번호 텍스트박스
+
+                
+                int capacity = 0;
+                if (!int.TryParse(rCapacitytxt.Text, out capacity))
+                {
+                    MessageBox.Show("수용 인원은 숫자로 입력해주세요.");
+                    return;
+                }
+
+                string cleaning_status = rCleaningtxt.SelectedItem.ToString(); // 청소 상태 텍스트박스
+
+                if (string.IsNullOrEmpty(roomNumber) || string.IsNullOrEmpty(status))
+                {
+                    MessageBox.Show("모든 항목을 입력해주세요.");
+                    return;
+                }
+
+                sqlstr = "SELECT COUNT(*) FROM room WHERE room_id = :room_id";
+                OracleCommand cmdCheck = new OracleCommand(sqlstr, dbc.Con);
+                cmdCheck.Parameters.Add("room_id", OracleDbType.Int32).Value = Convert.ToInt32(roomNumber);
+
+                int count = Convert.ToInt32(cmdCheck.ExecuteScalar());
+                if (count > 0)
+                {
+                    MessageBox.Show("이미 존재하는 방 번호입니다.");
+                    return;
+                }
+
+                sqlstr = "INSERT INTO room (room_id, status, capacity, cleaning_status, machine_id) VALUES (:room_id, :status, :capacity, :cleaning_status, :machine_id)";
+
+                OracleCommand cmd = new OracleCommand(sqlstr, dbc.Con);
+
+                cmd.Parameters.Add("room_id", OracleDbType.Int32).Value = Convert.ToInt32(roomNumber);
+                cmd.Parameters.Add("status", OracleDbType.Varchar2).Value = status;  // 방 상태
+                cmd.Parameters.Add("capacity", OracleDbType.Int32).Value = capacity;
+                cmd.Parameters.Add("cleaning_status", OracleDbType.Varchar2).Value = cleaning_status;  // 청소 상태
+                cmd.Parameters.Add("machine_id", OracleDbType.Int32).Value = Convert.ToInt32(machine_id); // 기계 번호
+
+                cmd.ExecuteNonQuery();
+
+                dbc.DS.Tables["room"].Clear();
+
+                sqlstr = "Select * From room ORDER BY room_id ASC";
+                dbc.DA.SelectCommand = dbc.DCom;
+                dbc.DA.Fill(dbc.DS, "room");
+                DataGridView1.DataSource = dbc.DS.Tables["room"].DefaultView;
+
+                rNumtxt.Clear();
+                rStatustxt.SelectedItem = null;
+                mNumtxt.Clear();
+                rCapacitytxt.Clear();
+                rCleaningtxt.SelectedItem = null;
+
+                MessageBox.Show("방이 성공적으로 추가되었습니다.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("추가 오류: " + ex.Message);
+            }
+        }
+
+
+
+
+
+        public void Backbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            main form1 = new main();
+            form1.Show(); // main 폼을 보이게 함
+        }
+
+
+
+
+
+
+
+    }
+}
